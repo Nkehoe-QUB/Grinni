Moving average fix

src/Grinni/_Process.py
@@ -149,10 +149,9 @@ class Process():
                     Z=1
                 elif "electron" in Name:
                     Z=1
-                axis_data = self.np.array(MetaData.getAxis('ekin', timestep=self.TimeSteps[0])/Z)
+                axis_data = MovingAverage(self.np.array(MetaData.getAxis('ekin', timestep=self.TimeSteps[0])/Z),3)
                 for t in self.TimeSteps[1:]:
-                    axis_data=self.np.vstack((axis_data, MetaData.getAxis('ekin', timestep=t)/Z))
-                axis_data = MovingAverage(axis_data, 3)
+                    axis_data=self.np.vstack((axis_data, MovingAverage(self.np.array(MetaData.getAxis('ekin', timestep=t)/Z),3)))
             elif axis_name == "px":
                 if "x-px" in Name:
                     bin_size = axis['x'][1]-axis['x'][0]